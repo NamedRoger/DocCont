c68dc8f3f83c21ed18703c4a17b2581cb201ace2
NamedRoger <roge.villa.carrera@hotmail.com>
2020-11-25 20:17:02 -0600

    separator

--- src/com/NamedRoger/infraestructura/Constante.java
+++ src/com/NamedRoger/infraestructura/Constante.java
@@ -5,7 +5,7 @@ import java.nio.file.FileSystems;
 public class Constante {
 
     public static String getPath() {
-        var separator = FileSystems.getDefault().getSeparator();
+        String separator = FileSystems.getDefault().getSeparator();
         String fileName = String.format("src%scom%sNamedRoger%sFiles%s",separator,separator,separator,separator);
 
         return fileName;
