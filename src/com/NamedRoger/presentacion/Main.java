cfc84409aca0ea30498ff73ec3d51ef6dce77ab9
kevs03 <kevinj03@hotmail.com>
2020-11-29 16:51:08 -0600

    Cambios sobre null en EspecialidadCreador

--- src/com/NamedRoger/presentacion/Main.java
+++ src/com/NamedRoger/presentacion/Main.java
@@ -6,10 +6,12 @@ import src.com.NamedRoger.dominio.db.DataBase;
 import src.com.NamedRoger.dominio.db.RegistroDataBase;
 import src.com.NamedRoger.dominio.models.EstatusCita;
 
-public class Main {
-    public static void main(String args[]) throws Exception {
-        DataBase db = DataBase.getInstance();
-        RegistroDataBase registroDataBase = RegistroDataBase.getInstance();
 
-    }
+public class Main {
+
+        public static void main(String args[]) throws Exception {
+            DataBase db = DataBase.getInstance();
+            RegistroDataBase registroDataBase = RegistroDataBase.getInstance();
+
+        }
 }
